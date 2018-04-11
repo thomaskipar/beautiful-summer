d1d0b2773e379d21580bb32e376edcd0db8dadc1
thomaskipar <thomas.kipar@gmail.com>
2018-04-11 15:24:04 +0200

    Added war-days-finder

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ let moment = require('moment');
 let config = require('../config/config.json');
 let baseUrl = 'http://api.weatherbit.io/v2.0/history/daily';
 let schedule = require('node-schedule');
+var express = require('express');
 
 let downloader = require('./data-downloader');
 let parser = require('./data-parser');
@@ -27,3 +28,15 @@ function updateDataModel() {
 console.log('Running backend for beautiful summer app');
 updateDataModel();
 
+var app = express();
+var port = 8080;
+
+app.use(express.static('../../src/frontend'));
+
+app.get('/api', function (req, res) {
+    res.json(config);
+});
+
+app.listen(8080, function () {
+    console.log(`Backend is listening on port ${port}`);
+});
---
+++ backend/src/warm-days-finder.js
@@ -0,0 +1,51 @@
+let LineByLineReader = require('line-by-line');
+let moment = require('moment');
+let parser = require('./data-parser');
+
+function findLongestWarmPeriod(data, year) {
+
+    let startDate = moment(`${year}-01-01`);
+    let endDate = moment(`${year}-12-31`);
+
+    let date = moment(startDate);
+
+    let isSummerPeriod = false;
+
+    let currentSummerDate;
+    let minSummerTemperature = 28;
+    let numSummerDays = 0;
+
+    let maxSummerDate;
+    let maxSummerDays = 0;
+
+    while (date.isBefore(endDate)) {
+        let temperature = data[date.format("YYYYMMDD")];
+        if (temperature >= minSummerTemperature) {
+            if (currentSummerDate) {
+                numSummerDays++;
+            } else {
+                currentSummerDate = moment(date);
+                numSummerDays = 1;
+            }
+        } else if (currentSummerDate) {
+            if (maxSummerDays < numSummerDays) {
+                maxSummerDate = currentSummerDate;
+                maxSummerDays = numSummerDays;
+            }
+            currentSummerDate = undefined;
+        }
+        date = date.add(1, 'days');
+    }
+
+    if (maxSummerDate) {
+        return {
+            date: maxSummerDate.format("YYYYMMDD"),
+            days: maxSummerDays
+        }
+    } else {
+        return undefined;
+    }
+}
+
+
+
